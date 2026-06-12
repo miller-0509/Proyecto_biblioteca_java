773a7466f0a13d943884d1bf4d192bc0a1efcaad
miller-0509 <caperamiller5@gmail.com>
2026-06-12 14:28:20 -0500

    Cambios

--- src/Vista/MDIBiblioteca.java
+++ src/Vista/MDIBiblioteca.java
@@ -29,11 +29,16 @@ public class MDIBiblioteca extends javax.swing.JFrame {
         menuBar = new javax.swing.JMenuBar();
         fileMenu = new javax.swing.JMenu();
         openMenuItem = new javax.swing.JMenuItem();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         fileMenu.setMnemonic('f');
-        fileMenu.setText("Gestión");
+        fileMenu.setText("Equipos");
 
         openMenuItem.setMnemonic('o');
         openMenuItem.setText("Abrir");
@@ -42,17 +47,32 @@ public class MDIBiblioteca extends javax.swing.JFrame {
 
         menuBar.add(fileMenu);
 
+        jMenu1.setText("Libros");
+        menuBar.add(jMenu1);
+
+        jMenu2.setText("Préstamos");
+        menuBar.add(jMenu2);
+
+        jMenu3.setText("Reportes");
+        menuBar.add(jMenu3);
+
+        jMenu4.setText("Sanciones");
+        menuBar.add(jMenu4);
+
+        jMenu5.setText("Usuarios");
+        menuBar.add(jMenu5);
+
         setJMenuBar(menuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(escritorio, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+            .addComponent(escritorio, javax.swing.GroupLayout.DEFAULT_SIZE, 827, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(escritorio, javax.swing.GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE)
+            .addComponent(escritorio, javax.swing.GroupLayout.DEFAULT_SIZE, 441, Short.MAX_VALUE)
         );
 
         pack();
@@ -100,6 +120,11 @@ public class MDIBiblioteca extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane escritorio;
     private javax.swing.JMenu fileMenu;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
     private javax.swing.JMenuBar menuBar;
     private javax.swing.JMenuItem openMenuItem;
     // End of variables declaration//GEN-END:variables
